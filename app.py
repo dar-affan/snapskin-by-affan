831726d192ef6d3342df40a1095c7952e2760991
dar-affan <onionispink@gmail.com>
2026-01-05 22:11:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run()
+[
+  {
+    "id": 1,
+    "name": "Anime Phone Skin",
+    "type": "physical",
+    "price": "399",
+    "image": "https://images.unsplash.com/photo-1585079542156-2755d9c8a094"
+  },
+  {
+    "id": 2,
+    "name": "Marble Luxury Skin",
+    "type": "physical",
+    "price": "499",
+    "image": "https://images.unsplash.com/photo-1607082349566-187342175e2f"
+  },
+  {
+    "id": 3,
+    "name": "Digital Anime Pack",
+    "type": "digital",
+    "price": "199",
+    "image": "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+  }
+]
